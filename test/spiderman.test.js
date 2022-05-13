a289adb9699352279c8da1f1ea7ac74915f53d2f
Monica Portillo <44446125+monicaps@users.noreply.github.com>
2022-05-13 01:30:32 -0500

    add proyecto spiderverse elaborado a traves de TDD

---
+++ test/spiderman.test.js
@@ -0,0 +1,19 @@
+const Spiderman = require('./../app/spiderman')
+
+describe("Unit Tests for Spiderman class", () => {
+  test('1) Create a spiderman object', () => {
+    const andrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield", 2, "Sony")
+    expect(andrewGarfield.name).toBe("Spiderman Sony");
+    expect(andrewGarfield.age).toBe(31);
+    expect(andrewGarfield.actor).toBe("Andrew Garfield");
+    expect(andrewGarfield.movies).toBe(2);
+    expect(andrewGarfield.studio).toBe("Sony");
+  });
+})
+
+describe("Unit Tests for Spiderman class", () => {
+  test('2) Use the method getInfo()', () => {
+    const tomHolland = new Spiderman("Spiderman Marvel",25,"Tom Holland", 5, "Marvel")
+    expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio");
+  });
+})
